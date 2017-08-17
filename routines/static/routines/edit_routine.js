8e0d2934e79d9fa174a7aee7e7eac6c7ee39ff0d
EdlanPolicarpio <edlanpolicarpioschool@gmail.com>
2017-08-17 23:39:45 +1000

    Fixed bugs with adding new workouts.

--- routines/static/routines/edit_routine.js
+++ routines/static/routines/edit_routine.js
@@ -1,10 +1,26 @@
-Sortable.create(foo, {group: 'foo',animation: 100});                           
-Sortable.create(bar, {group: 'bar',animation: 100});                           
+//Create Sortable for workouts
 var editableList = Sortable.create(wo_form_set, {
     filter: '.js-remove',
     onFilter: function (evt) {
             var el = editableList.closest(evt.item); // get dragged item
-            el && el.parentNode.removeChild(el);
+            var form = el.parentNode;
+            form.removeChild(el);
         }
-    });
-Sortable.create(qux, {group: {name: 'qux', put: ['foo', 'bar']},animation: 100});
+});
+//Function for creating a new form
+function add_form(appendTo, id, template){
+    var table_meta = id + '-TOTAL_FORMS';
+    var form_idx = $(table_meta).val();
+    var index = form_idx;
+    var wo_template = `<tr><th><label for="id_workout_set-${index}-name">Name:</label></th><td><input type="text" name="workout_set-${index}-name" maxlength="100" id="id_workout_set-${index}-name" /></td></tr>
+        <tr><th><label for="id_workout_set-${index}-day">Day:</label></th><td><input type="number" name="workout_set-${index}-day" value="0" min="0" id="id_workout_set-${index}-day" /></td></tr>
+        <tr><th><label for="id_workout_set-${index}-DELETE">Delete:</label></th><td><input type="checkbox" name="workout_set-${index}-DELETE" id="id_workout_set-${index}-DELETE" />
+        <input type="hidden" name="workout_set-${index}-id" id="id_workout_set-${index}-id" /><input type="hidden" name="workout_set-${index}-routine" value="1" id="id_workout_set-${index}-routine" /></td></tr>`
+    appendTo.append(wo_template);
+    $(table_meta).val(parseInt(form_idx) + 1);
+}
+//Add form event
+var wo_location = $('#wo_form');
+var wo_id = '#id_workout_set';
+var wo_template = `` 
+$('#add_form').click(function(){add_form(wo_location,wo_id, wo_template)});
