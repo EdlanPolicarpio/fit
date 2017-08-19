96965ac828b8b7246c3b5be2f8503f9c943959f6
EdlanPolicarpio <edlanpolicarpioschool@gmail.com>
2017-08-20 02:32:27 +1000

    adding workout prefills day. Re-ordering changes day. TODO:Rest days

--- routines/forms.py
+++ routines/forms.py
@@ -3,20 +3,24 @@ from django.forms import inlineformset_factory
 from .models import Routine, Workout, Excercise
 
 class ExcerciseForm(forms.ModelForm):
-	class Meta:
-		model = Excercise
-		fields = ['name','workout','reps','sets','order']
+        class Meta:
+                model = Excercise
+                fields = ['name','workout','reps','sets','order']
 
 class WorkoutForm(forms.ModelForm):
-	class Meta:
-		model = Workout
-		fields = ['name','day']
-
+        class Meta:
+               model = Workout
+               fields = ['name','day']
 class RoutineForm(forms.ModelForm):
-	class Meta:
-		model = Routine
-		fields = ['name','num_days','desc']
+        class Meta:
+                model = Routine
+                fields = ['name','num_days','desc']
 
 #Formsets
 
-WorkoutFormSet = inlineformset_factory(Routine, Workout, fields=('name','day',), extra = 0,can_delete=True)
+WorkoutFormSet = inlineformset_factory(Routine, Workout, fields=('name','day',), 
+        extra = 0, can_delete=True, 
+        widgets = {
+            'name':forms.TextInput(attrs={'class':'wo_name'}),
+             'day':forms.TextInput(attrs={'class':'wo_day'}), 
+            })
--- routines/static/routines/2.js
+++ routines/static/routines/2.js
@@ -1,40 +1,37 @@
-//Document Ready
-
+//For some reason, sortable index starts at 4
+const ROOT_INDEX = 4
 //Create Sortable for workouts
 var editableList = Sortable.create(wo_list, {
     sort: 'true',
     scroll: 'true',
     filter: '.js-remove',
     onFilter: function (evt) {
-        var el = editableList.closest(evt.item); // get dragged item
-        var form = el.parentNode;
-        $(el).find('.wo_DELETE')[0].checked = true;
-        $("#deleted").append(el);
-        },
-   onUpdate: function(evt){                                        
-       var list = $('#wo_list');                                   
-       list.children().each(function(){                                       
-           $(this).find('.wo_day').attr("value", $(this).index()); 
-       
-       });                                                         
-   }                                                               
+            var el = editableList.closest(evt.item); // get dragged item
+            var form = el.parentNode;
+            el.find('.wo_DELETE').checked = true;
+            $("#deleted").append(el);
+    },
+    onUpdate: function(evt){
+        var list = $("wo_form_set");
+        alert(list);
+    }
 });
 //Function for setting the day
-
+function order_workouts(wo_list){
+}
 //Function for creating a new form
 function add_form(appendTo, id, template){
     var table_meta = id + '-TOTAL_FORMS';
     var form_idx = $(table_meta).val();
     var index = form_idx;
-    var new_index = appendTo.children().length + 1;
     var wo_temp = `<!--new_wo_form--!>
-        <li class="to_sort"><div><table>
+        <li><div><table>
             <input type="hidden" name="workout_set-${index}-id" id="id" />
             <input type="hidden" name="workout_set-${index}-routine" value="${routine_id}" id="routine" />
-            <th><input type="number" value="${new_index}" name="workout_set-${index}-day" min="0" id="day" /></th>
+            <th><input type="number" name="workout_set-${index}-day" min="0" id="day" /></th>
             <th colspan = "4"><input type="text" name="workout_set-${index}-name" maxlength="100" id="name" /></th>
             <th class="js-remove">X</p>
-            <th style="display:none"> <input type="checkbox" name="workout_set-${index}-DELETE" id="DELETE" class="wo_DELETE"/></th>
+            <th> <input type="checkbox" name="workout_set-${index}-DELETE" id="DELETE" /></th>
       </li></table></div>
     `
     appendTo.append(wo_temp);
