dfcdb56de0dbcc39e0e614bad7d13df0fd010f51
EdlanPolicarpio <edlanpolicarpioschool@gmail.com>
2017-08-19 18:36:04 +1000

    fixed dynamic form removal. TODO: change 'wo_day' when reordered.

--- routines/static/routines/edit_routine.js
+++ routines/static/routines/edit_routine.js
@@ -1,10 +1,30 @@
+//For some reason, sortable index starts at 4
+const ROOT_INDEX = 4
 //Create Sortable for workouts
-var editableList = Sortable.create(wo_form_set, {
+var editableList = Sortable.create(wo_list, {
+    sort: 'true',
+    scroll: 'true',
     filter: '.js-remove',
     onFilter: function (evt) {
             var el = editableList.closest(evt.item); // get dragged item
             var form = el.parentNode;
-            form.removeChild(el);
+            var inputs = el.getElementsByTagName('input');
+            for( var i = 0; i <inputs.length; i++){
+                if( inputs[i].getAttribute('type') == 'checkbox'){
+                    inputs[i].checked = true;
+                }
+            }
+            $("#deleted").append(el);
+            //form.removeChild(el);
+    },
+    onUpdate: function(evt){
+        var inputs = evt.item.getElementsByTagName('input');
+        for(var i = 0; i<inputs.length; i++){
+            if( inputs[i].getAttribute('type') == 'number'){
+                inputs[i].value = evt.newIndex;
+            }
+        }
+        alert(evt.from);
     }
 });
 //Function for creating a new form
@@ -12,28 +32,17 @@ function add_form(appendTo, id, template){
     var table_meta = id + '-TOTAL_FORMS';
     var form_idx = $(table_meta).val();
     var index = form_idx;
-    var wo_template = `<!--new_wo_form --!>
-    <div id="workout"><table>
-    <p class="js-remove"> remove </p>
-    <tr>
-      <th><label for="id_workout_set-${index}-name">Name:</label></th>
-      <td><input type="text" name="workout_set-${index}-name" maxlength="100" id="id_workout_set-${index}-name" /></td>
-    </tr>
-    <tr>
-      <th><label for="id_workout_set-${index}-day">Day:</label></th>
-      <td><input type="number" name="workout_set-${index}-day" value="0" min="0" id="id_workout_set-${index}-day" /></td>
-    </tr>
-    <tr>
-      <th><label for="id_workout_set-${index}-DELETE">Delete:</label></th>
-      <td>
-        <input type="checkbox" name="workout_set-${index}-DELETE" id="id_workout_set-${index}-DELETE" />
-        <input type="hidden" name="workout_set-${index}-id" id="id_workout_set-${index}-id" />
-        <input type="hidden" name="workout_set-${index}-routine" value="1" id="id_workout_set-${index}-routine" />
-      </td>
-    </tr>
-    </table></div>
+    var wo_temp = `<!--new_wo_form--!>
+        <div id="workout"><table>
+            <input type="hidden" name="workout_set-${index}-id" id="id" />
+            <input type="hidden" name="workout_set-${index}-routine" value="${routine_id}" id="routine" />
+            <th><input type="number" name="workout_set-${index}-day" min="0" id="day" /></th>
+            <th colspan = "4"><input type="text" name="workout_set-${index}-name" maxlength="100" id="name" /></th>
+            <th class="js-remove">X</p>
+            <th> <input type="checkbox" name="workout_set-${index}-DELETE" id="DELETE" /></th>
+      </table></div>
     `
-    appendTo.append(wo_template);
+    appendTo.append(wo_temp);
     $(table_meta).val(parseInt(form_idx) + 1);
 }
 //Add form event
