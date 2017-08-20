ff86b2bd2b1bb4d640348d5385f46a8ab8348419
EdlanPolicarpio <edlanpolicarpioschool@gmail.com>
2017-08-20 17:30:13 +1000

    Sorts Workouts. Creates 'Rest Day' placeholders.

--- routines/static/routines/edit_routine.js
+++ routines/static/routines/edit_routine.js
@@ -1,5 +1,21 @@
 //Document Ready
-
+$(document).ready(function(){
+    var sorted_wo =  $('#wo_list').children().sort(wo_sort); 
+    sorted_wo.appendTo('#wo_list');
+    var max_day = sorted_wo.last().find('.wo_day').attr('value');
+    var curr = sorted_wo.first();
+    var val = parseInt(curr.find('.wo_day').attr('value'));
+    for(var i= 0; i< max_day; i++){
+        if(val === i){
+            curr = curr.next();
+            val = parseInt(curr.find('.wo_day').attr('value'));
+        }else{
+            add_rest(i);
+        }
+    }
+    var sorted_wo =  $('#wo_list').children().sort(wo_sort); 
+    sorted_wo.appendTo('#wo_list');
+});
 //Create Sortable for workouts
 var editableList = Sortable.create(wo_list, {
     sort: 'true',
@@ -19,7 +35,11 @@ var editableList = Sortable.create(wo_list, {
        });                                                         
    }                                                               
 });
-//Function for setting the day
+
+//Workout Sorting
+function wo_sort(a,b){
+    return ($(b).find('.wo_day').attr('value') < $(a).find('.wo_day').attr('value')) ? 1:-1;
+}
 
 //Function for creating a new form
 function add_form(appendTo, id, template){
@@ -33,15 +53,36 @@ function add_form(appendTo, id, template){
             <input type="hidden" name="workout_set-${index}-routine" value="${routine_id}" id="routine" />
             <th><input type="number" value="${new_index}" name="workout_set-${index}-day" min="0" id="day" /></th>
             <th colspan = "4"><input type="text" name="workout_set-${index}-name" maxlength="100" id="name" /></th>
-            <th class="js-remove">X</p>
+            <th class="js-remove">X</th>
             <th style="display:none"> <input type="checkbox" name="workout_set-${index}-DELETE" id="DELETE" class="wo_DELETE"/></th>
       </li></table></div>
     `
     appendTo.append(wo_temp);
     $(table_meta).val(parseInt(form_idx) + 1);
 }
+
+//Add Rest Day
+function add_rest(day){
+    var workouts = $('#wo_list');
+    var html = `<!--rest_day--!>
+        <li class="to_sort"><div><table>
+          <th><input type="number" value="${day}" class="wo_day"></th>
+          <th>Rest Day</th>
+          <th class="js-remove">X</th>
+        </table></div></li>
+        
+        `
+    workouts.append(html);
+    
+}
 //Add form event
 var wo_location = $('#wo_list');
 var wo_id = '#id_workout_set';
 var wo_template = `` 
 $('#add_form').click(function(){add_form(wo_location,wo_id, wo_template)});
+
+//Add rest day
+$('#add_rest').click(function(){
+    var new_index = $('#wo_list').children().length + 1;
+    add_rest(new_index);
+});
